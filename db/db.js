bc6b6d9eeb1e5e36be7ba86b524112d2367dd076
safi <safiqureshi531@gmail.com>
2024-01-06 15:01:55 +0530

    first

---
+++ db/db.js
@@ -0,0 +1,308 @@
+export default {
+	bio: {
+		about: {
+			text: [
+				"Hi &#128075;",
+				"I'm Safi-Ur-Rehman. I’m currently a student at Jaypee University of Engineering & Technology specialized in Computer Science and Engineering.",
+				"I'm a Web Developer, geek and curious human besides being an DSA enthusiast. I have experience of delivering solutions for interesting problems.",
+				"I love to read, listen to music, and travel.",
+			],
+		},
+		contact: {
+			text: [
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: jshreyansh459@gmail.com",
+			],
+		},
+	},
+	skills: [
+		{
+			title: "Languages",
+			skillName: "C, C++",
+			color: "1",
+			percentage: "90",
+		},
+		{
+			title: "Frameworks/Libraries",
+			skillName: "Reactjs",
+			color: "6",
+			percentage: "80",
+		},
+		{
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
+			color: "2",
+			percentage: "50",
+		},
+		{
+			title: "Clouds",
+			skillName: "Google Cloud, Netlify",
+			color: "3",
+			percentage: "30",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, CSS",
+			color: "4",
+			percentage: "90",
+		},
+		{
+			title: "Version Control",
+			skillName: "Git, GitHub",
+			color: "7",
+			percentage: "70",
+		},
+		{
+			title: "Tools",
+			skillName: "Chrome DevTools",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "Editor",
+			skillName: "Visual Code Studio",
+			color: "6",
+			percentage: "80",
+		},
+	],
+	projects: {
+		web: [
+			 {
+			 	projectName: "Youtube Clone",
+			 	image: "images/programmingdiaries.png",
+			 	summary:
+			 		"Developed a frontend of the well known Youtube website where various details of videos are shown.",
+			 	preview: "https://github.com/safiQureshi531/Youtube-Clone",
+			 	techStack: [
+			 		"HTML",
+			 		"Bootstrap","ReactJS",
+			 		"JavaScript",
+			 	],
+			 },
+			{
+				projectName: "Weather App",
+				image: "images/findyourbank.png",
+				summary: "Well built website to check your weather status of your desires location fetching weather details from weather API.",
+				preview: "https://github.com/safiQureshi531/Weather-Forecasting-",
+				techStack: ["HTML","CSS", "Bootstrap", "JavaScript"],
+			},
+			{
+				projectName: "Whatsapp Chat Analyser",
+				image: "images/portfolio.png",
+				summary: "It is a statistical analysis tool for WhatsApp chats. Working on the chat files that can be exported from WhatsApp. It generates various plots showing, for example, which another participant a user responds to the most. It is made completely using Python.",
+				preview: "https://github.com/safiQureshi531/Whatsapp-Chat-Analyse",
+				techStack: ["Python"],
+			},
+			{
+				projectName: "Spotify Clone",
+				image: "images/resume-builder.png",
+				summary:
+					"A clone for world famous spotify application to play music.",
+				preview: "https://github.com/safiQureshi531/Spotify-clone",
+				techStack: ["HTML", "CSS", "Bootstrap", "JavaScript"],
+			},
+		],
+		software: [
+		//	 {
+		//	 	projectName: "BLW Inventory Shop",
+		//	 	image: "images/pizzaorderchatbot.png",
+		//	 	summary:
+		//	 		"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
+		//	 	preview:
+		//	 		"https://github.com/shreyaaansh/RailwayProductSorter",
+		//	 	techStack: ["Dailogflow", "Firebase"],
+		//	 },
+		//	 {
+		//	 	projectName: "Expense Tracker",
+		//	 	image: "images/whatsappbot.jpg",
+		//	 	summary: "A platform to keep your daily expenses record and spend accordingly.",
+		//	 	preview: "https://github.com/shreyaaansh/Expense-Tracker",
+		//	 	techStack: ["Selenium", "Chrome Webdriver", "Python"],
+		//	 },
+		//	 {
+		//	 	projectName: "Connect-Ho!",
+		//	 	image: "images/billgenerator.png",
+		//	 	summary: "A clone for social media website where useres interact and has functionality",
+		//	 	preview: "https://github.com/vinaysomawat/Bill-Generator",
+			 	//techStack: ["Tkinter", "Openxlpy", "Python"],
+			 //},
+		],
+		android: [
+			 //{
+			 //	projectName: "AI baseed Attendance System",
+			 	//image: "images/nitwcse.jpg",
+			 	//summary: "The Application shows the real time attendance of all the registered students with exact timeing of particular date.",
+			 //	preview: "https://github.com/shreyaaansh/AI-based-Attendance-System",
+			 	//techStack: ["Python","OpenCV","NumPy", "Face-Recognition", "CSV","Pandas"],
+			 //},
+			 //{
+			 //	projectName: "CareerHigh-App",
+			 //	image: "images/carrerhigh.png",
+			 //	summary:
+			 //		"The Application display the webpages of website careerhigh.in in android devices.",
+			 //	preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+			 //	techStack: ["JAVA", "XML", "Android"],
+			 //},
+		],
+		freelance: [
+			// {
+			// 	projectName: "SnylloAir.com",
+			// 	image: "images/snylloair.png",
+			// 	summary:
+			// 		"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+			// 	preview: "https://www.snylloair.com/",
+			// 	techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			// },
+			// {
+			// 	projectName: "Delivery+",
+			// 	image: "images/AM-Logo-.png",
+			// 	summary:
+			// 		"Android Application to display website in android devices.",
+			// 	preview:
+			// 		"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+			// 	techStack: ["Android", "JAVA", "Play Store"],
+			// },
+		],
+	},
+	experience: [
+		{
+			title: "Gas Authority of India Limited (GAIL)",
+			duration: "July 2023 - August 2023",
+			subtitle: "Computer Science Intern",
+			details: [
+				"Developed a full-fledged working project named PG booking website using technologies like HTML, CSS, and JavaScript for the front end and MYSQL for the back end."
+			],
+			tags: [
+				"HTML",
+			   "CSS",
+			   "JavaScript",
+				"React",
+				"Bootstrap",
+				"MYSQL",
+			],
+		   icon: "heartbeat",
+		},
+		 {
+		 	title: "InternShala Trainee",
+		 	duration: "June 2023 - July 2023",
+		 	subtitle: "Computer Science Intern",
+		 	details: [
+		 		"Successfully completed an 8-week online training on Web Development. The training consisted of HTML, CSS, Bootstrap, DBMS, JS, React, and Final Project modules."
+		 	],
+		 	tags: [
+		 		"HTML",
+				"CSS",
+				"JavaScript",
+		 		"React",
+		 		"Bootstrap",
+		 		"Nodejs",
+		 		"MYSQL",
+		 	],
+			icon: "heartbeat",
+		}
+		
+		// {
+		// 	title: "Novopay Solutions Pvt. Ltd.",
+		// 	duration: "June 2020 - April 2022",
+		// 	subtitle: "Software Engineer",
+		// 	details: [
+		// 		"",
+		// 		
+		// 	],
+		// 	tags: [
+		// 		"JavaScript",
+		// 		"Angular",
+		// 		"React",
+		// 		"Bootstrap",
+		// 		"Nodejs",
+		// 		"Jenkins",
+		// 	],
+		// 	icon: "qrcode",
+		// },
+		
+	],
+	education: [
+		{
+			title: "Bachelors in Computer Science and Engineering",
+			duration: "2020 - 2024",
+			subtitle: "Jaypee University of Engineering and Technology, Guna",
+			details: [
+				"Current CGPA 6.3",
+				"Active Competitive Programmer with CodeChef and Leetcode ratings*.",
+				"Head of lots of college's different types of clubs",
+			],
+			tags: [
+				"Data Structures & Algorithms",
+				"Operating Systems",
+				"Database Management System",
+				"Computer Networks",
+				"Mobile Computing",
+				"Artificial Intelligence",
+				"Theory Of Computation",
+			],
+			icon: "graduation-cap",
+		},
+		{
+			title: "Senior Secondary in Science and Mathematics",
+			duration: "2018 - 2019",
+			subtitle: "Modern Children Higher Secondary School, Guna",
+			details: [
+				"Secured 77 percentage in Class 12th Boards Examinations.",
+			],
+			tags: ["Physics", "Chemistry", "Mathematics"],
+			icon: "book",
+		},
+	],
+	footer: [
+		{
+			label: "Dev Profiles",
+			data: [
+				{
+					text: "LinkedIn",
+					link: "https://www.linkedin.com/in/safi-qureshi-b2345b277/",
+				},
+				{
+					text: "GitHub",
+					link: "https://github.com/safiQureshi531",
+				},
+				{
+					text: "LeetCode",
+					link: "https://leetcode.com/safiqureshi531/",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				// {
+				// 	text: "Print this page",
+				// 	func: "window.print()",
+				// },
+			],
+		},
+		{
+			label: "Social Profiles",
+			data: [
+				{
+					text: "Linkedin",
+					link: "https://www.linkedin.com/in/safi-qureshi-b2345b277/",
+				},
+				{
+					text: "Gmail",
+				 	link: "safiqureshi531@gmail.com",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"",
+				"Made with &hearts;",
+				"by Safi-Ur-Rehman."
+			],
+		},
+	],
+};
